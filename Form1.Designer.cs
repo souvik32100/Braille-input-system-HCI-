ae383d9da6ddfc5a7b742095cbebeaa6622d182b
souvik32100 <souvik32100@gmail.com>
2020-05-30 11:16:39 +0600

    Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace HCI
+{
+    partial class Keybord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.enter = new System.Windows.Forms.Button();
+            this.key6 = new System.Windows.Forms.Button();
+            this.key3 = new System.Windows.Forms.Button();
+            this.space = new System.Windows.Forms.Button();
+            this.key5 = new System.Windows.Forms.Button();
+            this.key2 = new System.Windows.Forms.Button();
+            this.key1 = new System.Windows.Forms.Button();
+            this.backspace = new System.Windows.Forms.Button();
+            this.key4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(895, 702);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Tan;
+            this.panel2.Controls.Add(this.enter);
+            this.panel2.Controls.Add(this.key6);
+            this.panel2.Controls.Add(this.key3);
+            this.panel2.Controls.Add(this.space);
+            this.panel2.Controls.Add(this.key5);
+            this.panel2.Controls.Add(this.key2);
+            this.panel2.Controls.Add(this.key1);
+            this.panel2.Controls.Add(this.backspace);
+            this.panel2.Controls.Add(this.key4);
+            this.panel2.Location = new System.Drawing.Point(312, 360);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 255);
+            this.panel2.TabIndex = 2;
+            // 
+            // enter
+            // 
+            this.enter.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter.Location = new System.Drawing.Point(181, 176);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(61, 55);
+            this.enter.TabIndex = 16;
+            this.enter.Text = "E";
+            this.enter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.enter.UseVisualStyleBackColor = true;
+            // 
+            // key6
+            // 
+            this.key6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key6.Location = new System.Drawing.Point(98, 176);
+            this.key6.Name = "key6";
+            this.key6.Size = new System.Drawing.Size(61, 55);
+            this.key6.TabIndex = 15;
+            this.key6.Text = "*";
+            this.key6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key6.UseVisualStyleBackColor = true;
+            // 
+            // key3
+            // 
+            this.key3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key3.Location = new System.Drawing.Point(14, 176);
+            this.key3.Name = "key3";
+            this.key3.Size = new System.Drawing.Size(61, 55);
+            this.key3.TabIndex = 14;
+            this.key3.Text = "*";
+            this.key3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key3.UseVisualStyleBackColor = true;
+            // 
+            // space
+            // 
+            this.space.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.space.Location = new System.Drawing.Point(181, 94);
+            this.space.Name = "space";
+            this.space.Size = new System.Drawing.Size(61, 55);
+            this.space.TabIndex = 13;
+            this.space.Text = "_";
+            this.space.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.space.UseVisualStyleBackColor = true;
+            // 
+            // key5
+            // 
+            this.key5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key5.Location = new System.Drawing.Point(98, 94);
+            this.key5.Name = "key5";
+            this.key5.Size = new System.Drawing.Size(61, 55);
+            this.key5.TabIndex = 12;
+            this.key5.Text = "*";
+            this.key5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key5.UseVisualStyleBackColor = true;
+            this.key5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // key2
+            // 
+            this.key2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key2.Location = new System.Drawing.Point(14, 94);
+            this.key2.Name = "key2";
+            this.key2.Size = new System.Drawing.Size(61, 55);
+            this.key2.TabIndex = 11;
+            this.key2.Text = "*";
+            this.key2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key2.UseVisualStyleBackColor = true;
+            // 
+            // key1
+            // 
+            this.key1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key1.Location = new System.Drawing.Point(14, 14);
+            this.key1.Name = "key1";
+            this.key1.Size = new System.Drawing.Size(61, 55);
+            this.key1.TabIndex = 10;
+            this.key1.Text = "*";
+            this.key1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key1.UseVisualStyleBackColor = true;
+            // 
+            // backspace
+            // 
+            this.backspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backspace.Location = new System.Drawing.Point(181, 14);
+            this.backspace.Name = "backspace";
+            this.backspace.Size = new System.Drawing.Size(61, 55);
+            this.backspace.TabIndex = 3;
+            this.backspace.Text = "<";
+            this.backspace.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.backspace.UseVisualStyleBackColor = true;
+            this.backspace.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // key4
+            // 
+            this.key4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.key4.Location = new System.Drawing.Point(98, 14);
+            this.key4.Name = "key4";
+            this.key4.Size = new System.Drawing.Size(61, 55);
+            this.key4.TabIndex = 2;
+            this.key4.Text = "*";
+            this.key4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.key4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(76, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(728, 288);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // Keybord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 702);
+            this.Controls.Add(this.panel1);
+            this.Name = "Keybord";
+            this.Text = "Keybord";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keybord_KeyDown_1);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Button key6;
+        private System.Windows.Forms.Button key3;
+        private System.Windows.Forms.Button space;
+        private System.Windows.Forms.Button key5;
+        private System.Windows.Forms.Button key2;
+        private System.Windows.Forms.Button key1;
+        private System.Windows.Forms.Button backspace;
+        private System.Windows.Forms.Button key4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace HCI
+{
+    public partial class Keybord : Form
+    {
+        public Keybord()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue >= 96 && e.KeyValue <= 105)
+            {
+
+                
+                if (e.KeyValue == 103)
+                {
+                    key1.BackColor = Color.Aqua;
+                    
+                }
+                if (e.KeyValue==100)
+                {
+                    key2.BackColor = Color.Aqua;
+                    
+                }
+                
+                if (e.KeyValue == 97)
+                {
+                    key3.BackColor = Color.Aqua;
+                    
+                }
+                if (e.KeyValue == 104)
+                {
+                    key4.BackColor = Color.Aqua;
+                    
+                }
+                if (e.KeyValue == 101)
+                {
+                    key5.BackColor = Color.Aqua;
+                    
+                }
+                if (e.KeyValue == 98)
+                {
+                    key6.BackColor = Color.Aqua;
+                    
+
+                }
+                if (e.KeyValue == 105)
+                {
+                    backBtn();
+
+                }
+                if (e.KeyValue == 102)
+                {
+                    spaceBtn();
+
+                }
+                if (e.KeyValue == 99)
+                {
+                    enterBtn();
+
+                }
+                if (e.KeyValue == 96)
+                {
+                    print();
+                    update();
+                    
+                }
+
+
+
+            }
+        }
+
+        private void Keybord_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        public void update()
+        {
+            key1.BackColor = Color.White;
+            key2.BackColor = Color.White;
+            key3.BackColor = Color.White;
+            key4.BackColor = Color.White;
+            key5.BackColor = Color.White;
+            key6.BackColor = Color.White;
+        }
+        public void print()
+        {
+            if(key1.BackColor.Equals(Color.Aqua)&& key2.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "A";
+                //playSound();
+            }
+            if (key1.BackColor.Equals(Color.Aqua)&& key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "B";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White)&& key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "C";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "D";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.White) && key2.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "E";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "F";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "G";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "H";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "I";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "J";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "K";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "L";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key2.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "M";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.White) && key2.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "N";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "O";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "P";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "Q";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "R";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "S";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.White) && key6.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "T";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "U";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "V";
+            }
+            if (key2.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key1.BackColor.Equals(Color.White) && key3.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "W";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key5.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "X";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key4.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "Y";
+            }
+            if (key1.BackColor.Equals(Color.Aqua) && key3.BackColor.Equals(Color.Aqua) && key5.BackColor.Equals(Color.Aqua) && key6.BackColor.Equals(Color.Aqua) && key2.BackColor.Equals(Color.White) && key4.BackColor.Equals(Color.White))
+            {
+                richTextBox1.Text = richTextBox1.Text + "Z";
+            }
+        }
+
+
+
+        public void backBtn()
+        {
+            string x = richTextBox1.Text;
+            if (x.Length > 0) { x = x.Remove(x.Length - 1); }
+            
+            richTextBox1.Text=x;
+        }
+
+        public void spaceBtn()
+        {
+            richTextBox1.Text = richTextBox1.Text + " ";
+        }
+        
+        public void enterBtn()
+        {
+            richTextBox1.Text = richTextBox1.Text + "\n";
+        }
+
+        public void playSound()
+        {
+            SoundPlayer s = new SoundPlayer(@"C:\Users\SOUVIK\source\repos\HCI\HCI\audio-alphabet\A.wave");
+            s.Play();
+        }
+    }
+}
